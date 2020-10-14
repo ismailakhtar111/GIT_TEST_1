e6bf985e87fa21c8c9d22fa6164701601e75e277
ismail.akhtar <ismail.akhtar@ptw.com>
2020-10-14 10:14:37 +0530

    committed signupactivity

---
+++ app/src/main/java/com/demo/gittest/SignupActivity.java
@@ -0,0 +1,17 @@
+package com.demo.gittest;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Ismail.Akhtar on 14-10-2020.
+ */
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
